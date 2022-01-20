ebf8e39a8bb1a62c65ac54845a99005e5aa1bd89
JOJOKE21 <kingjordan2122@gmail.com>
2022-01-20 12:46:52 -0500

    fix: typeos

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -17,15 +17,16 @@ export default function Portfolio() {
     </div>
     <div className='tex3'>
          <h2 className='cent'>Space Rat!</h2> 
-       <p className='cent'>This is the project were we had to make a game in the course of little over a week. My game consept is to survive as long as possible fighting against
-       a wave of chesse. Get hit three times and you lose. Try your best to get a high score. 
+       <p className='cent'>This is the project where we had to make a game in the course of little over a week. My game concept is to survive as long as possible fighting against
+       a wave of cheese. Get hit three times and you lose. Try your best to get a high score.
        </p> 
        <p className='cent'>Langueges used: HTML, CSS, JavaScript</p>
     </div>
     <div className='tex4'>
     <h2 className='cent'>Calculator</h2>
-    <p className='cent'>This is the project were we had to make a working calculator. Using the knolege that we learned so far 
-      we had to make a funcioning calculator and we also had to make it unique to us the person making the calculator.
+    <p className='cent'>This is the project where we had to make a working calculator. Using the knowledge that we learned so far
+      we had to make a functioning calculator and we also had to make it unique to us the person making the calculator.
+
     </p> 
     <p className='cent'>Langueges used: HTML, CSS, JavaScript</p>
     </div>
